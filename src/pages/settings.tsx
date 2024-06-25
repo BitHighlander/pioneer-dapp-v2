79a6756105ad9fd2fcef7fd9d74b7465e5813e64
bithighlander <bithighlander@gmail.com>
2024-06-25 14:08:49 -0600

    broke

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
-import PageHeader from '@/components/PageHeader'
-import RelayRegionPicker from '@/components/RelayRegionPicker'
-import SettingsStore from '@/store/SettingsStore'
-import { cosmosWallets } from '@/utils/CosmosWalletUtil'
-import { eip155Wallets } from '@/utils/EIP155WalletUtil'
-import { solanaWallets } from '@/utils/SolanaWalletUtil'
-import { multiversxWallets } from '@/utils/MultiversxWalletUtil'
-import { tronWallets } from '@/utils/TronWalletUtil'
-import { kadenaWallets } from '@/utils/KadenaWalletUtil'
-import { Card, Col, Divider, Row, Switch, Text, Input, Button } from '@nextui-org/react'
-import { Fragment } from 'react'
-import { useSnapshot } from 'valtio'
-import packageJSON from '../../package.json'
-import { tezosWallets } from '@/utils/TezosWalletUtil'
+import { useState, Fragment } from 'react';
+import { Card, Col, Divider, Row, Switch, Text, Input, Button } from '@nextui-org/react';
+import { useSnapshot } from 'valtio';
+import PageHeader from '@/components/PageHeader';
+import RelayRegionPicker from '@/components/RelayRegionPicker';
+import SettingsStore from '@/store/SettingsStore';
+import { cosmosWallets } from '@/utils/CosmosWalletUtil';
+import { eip155Wallets } from '@/utils/EIP155WalletUtil';
+import { solanaWallets } from '@/utils/SolanaWalletUtil';
+import { multiversxWallets } from '@/utils/MultiversxWalletUtil';
+import { tronWallets } from '@/utils/TronWalletUtil';
+import { kadenaWallets } from '@/utils/KadenaWalletUtil';
+import { tezosWallets } from '@/utils/TezosWalletUtil';
 import { set, get, del, keys } from 'idb-keyval';
-import { decryptAES } from '@/utils/crypto' // Adjust the import path as needed
+import { decryptAES } from '@/utils/crypto';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import packageJSON from '../../package.json';
 
 export default function SettingsPage() {
     const {
@@ -31,15 +32,15 @@ export default function SettingsPage() {
         kernelSmartAccountEnabled,
         safeSmartAccountEnabled,
         biconomySmartAccountEnabled,
-        moduleManagementEnabled
-    } = useSnapshot(SettingsStore.state)
+        moduleManagementEnabled,
+    } = useSnapshot(SettingsStore.state);
 
-    const [password, setPassword] = useState('')
-    const [isAuthenticated, setIsAuthenticated] = useState(false)
-    const [seedPhrase, setSeedPhrase] = useState('')
+    const [password, setPassword] = useState('');
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [seedPhrase, setSeedPhrase] = useState('');
 
     const handlePasswordSubmit = async () => {
-        let tag = "SettingsPage | handlePasswordSubmit | "
+        let tag = 'SettingsPage | handlePasswordSubmit | ';
         try {
             const storedEncryptedPrivateKey = await get('encryptedPrivateKey');
             if (storedEncryptedPrivateKey) {
@@ -49,53 +50,47 @@ export default function SettingsPage() {
                         setSeedPhrase(decryptedSeedPhrase);
                         setIsAuthenticated(true);
                     } else {
-                        alert("Invalid password. Please try again.");
+                        alert('Invalid password. Please try again.');
                     }
                 } catch (error) {
-                    alert("Invalid password. Please try again.");
+                    alert('Invalid password. Please try again.');
                 }
             } else {
-                alert("No backup found.");
+                alert('No backup found.');
             }
         } catch (e) {
-            console.error(tag, e)
+            console.error(tag, e);
         }
-    }
+    };
 
     return (
         <Fragment>
             <PageHeader title="Settings" />
-
             {!isAuthenticated ? (
                 <Fragment>
-                    <Text h4 css={{ marginBottom: '$5' }}>
-                        Enter Password to Access Settings
-                    </Text>
-                    <Row justify="center" align="center">
-                        <Input
-                            type="password"
-                            placeholder="Password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                        <Button onClick={handlePasswordSubmit}>Submit</Button>
+                    <Row justify="center" align="center" css={{ height: '100vh', flexDirection: 'column' }}>
+                        <FontAwesomeIcon icon={faLock} size="3x" style={{ marginBottom: '20px' }} />
+                        <Text h4 css={{ marginBottom: '$5' }}>Enter Password to Access Settings</Text>
+                        <Row justify="center" align="center">
+                            <Input
+                                type="password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <Button onClick={handlePasswordSubmit}>Submit</Button>
+                        </Row>
                     </Row>
                 </Fragment>
             ) : (
                 <Fragment>
-                    <Text h4 css={{ marginBottom: '$5' }}>
-                        Packages
-                    </Text>
+                    <Text h4 css={{ marginBottom: '$5' }}>Packages</Text>
                     <Row justify="space-between" align="center">
                         <Text color="$gray400">@walletconnect/sign-client</Text>
                         <Text color="$gray400">{packageJSON.dependencies['@walletconnect/web3wallet']}</Text>
                     </Row>
-
                     <Divider y={2} />
-
-                    <Text h4 css={{ marginBottom: '$5' }}>
-                        Testnets
-                    </Text>
+                    <Text h4 css={{ marginBottom: '$5' }}>Testnets</Text>
                     <Row justify="space-between" align="center">
                         <Switch
                             checked={testNets}
@@ -104,14 +99,10 @@ export default function SettingsPage() {
                         />
                         <Text>{testNets ? 'Enabled' : 'Disabled'}</Text>
                     </Row>
-
                     <Divider y={2} />
-
                     <Row>
                         <Col>
-                            <Text h4 css={{ marginBottom: '$5' }}>
-                                Smart Accounts
-                            </Text>
+                            <Text h4 css={{ marginBottom: '$5' }}>Smart Accounts</Text>
                             {testNets ? (
                                 <>
                                     <Row justify="space-between" align="center">
@@ -122,12 +113,9 @@ export default function SettingsPage() {
                                         />
                                         <Text>{smartAccountEnabled ? 'Enabled' : 'Disabled'}</Text>
                                     </Row>
-
                                     {smartAccountEnabled ? (
                                         <>
-                                            <Text h4 css={{ marginBottom: '$5', marginTop: '$5' }}>
-                                                ZeroDev Smart Account
-                                            </Text>
+                                            <Text h4 css={{ marginBottom: '$5', marginTop: '$5' }}>ZeroDev Smart Account</Text>
                                             <Row justify="space-between" align="center">
                                                 <Switch
                                                     checked={kernelSmartAccountEnabled}
@@ -136,10 +124,7 @@ export default function SettingsPage() {
                                                 />
                                                 <Text>{kernelSmartAccountEnabled ? 'Enabled' : 'Disabled'}</Text>
                                             </Row>
-
-                                            <Text h4 css={{ marginBottom: '$5', marginTop: '$5' }}>
-                                                Safe Smart Account
-                                            </Text>
+                                            <Text h4 css={{ marginBottom: '$5', marginTop: '$5' }}>Safe Smart Account</Text>
                                             <Row justify="space-between" align="center">
                                                 <Switch
                                                     checked={safeSmartAccountEnabled}
@@ -148,10 +133,7 @@ export default function SettingsPage() {
                                                 />
                                                 <Text>{safeSmartAccountEnabled ? 'Enabled' : 'Disabled'}</Text>
                                             </Row>
-
-                                            <Text h4 css={{ marginBottom: '$5', marginTop: '$5' }}>
-                                                Biconomy Smart Account
-                                            </Text>
+                                            <Text h4 css={{ marginBottom: '$5', marginTop: '$5' }}>Biconomy Smart Account</Text>
                                             <Row justify="space-between" align="center">
                                                 <Switch
                                                     checked={biconomySmartAccountEnabled}
@@ -160,10 +142,7 @@ export default function SettingsPage() {
                                                 />
                                                 <Text>{biconomySmartAccountEnabled ? 'Enabled' : 'Disabled'}</Text>
                                             </Row>
-
-                                            <Text h4 css={{ marginBottom: '$5', marginTop: '$5' }}>
-                                                Sponsorship (Pimlico)
-                                            </Text>
+                                            <Text h4 css={{ marginBottom: '$5', marginTop: '$5' }}>Sponsorship (Pimlico)</Text>
                                             <Row justify="space-between" align="center">
                                                 <Switch
                                                     checked={smartAccountSponsorshipEnabled}
@@ -173,9 +152,7 @@ export default function SettingsPage() {
                                                 <Text>{smartAccountSponsorshipEnabled ? 'Enabled' : 'Disabled'}</Text>
                                             </Row>
                                             <Divider y={2} />
-                                            <Text h4 css={{ marginBottom: '$5', cursor: 'pointer' }}>
-                                                Module Management
-                                            </Text>
+                                            <Text h4 css={{ marginBottom: '$5', cursor: 'pointer' }}>Module Management</Text>
                                             <Row justify="space-between" align="center">
                                                 <Switch
                                                     disabled={
@@ -197,79 +174,42 @@ export default function SettingsPage() {
                             )}
                         </Col>
                     </Row>
-
                     <Divider y={2} />
-
                     <Row justify="space-between" align="center">
-                        <Text h4 css={{ marginBottom: '$5' }}>
-                            Relayer Region
-                        </Text>
+                        <Text h4 css={{ marginBottom: '$5' }}>Relayer Region</Text>
                         <RelayRegionPicker />
                     </Row>
-
                     <Divider y={2} />
-
                     <Text css={{ color: '$yellow500', marginBottom: '$5', textAlign: 'left', padding: 0 }}>
-                        Warning: mnemonics and secret keys are provided for development purposes only and should
-                        not be used elsewhere!
+                        Warning: mnemonics and secret keys are provided for development purposes only and should not be used elsewhere!
                     </Text>
-
-                    <Text h4 css={{ marginTop: '$5', marginBottom: '$5' }}>
-                        EIP155 Mnemonic
-                    </Text>
+                    <Text h4 css={{ marginTop: '$5', marginBottom: '$5' }}>EIP155 Mnemonic</Text>
                     <Card bordered borderWeight="light" css={{ minHeight: '100px' }}>
                         <Text css={{ fontFamily: '$mono' }}>{eip155Wallets[eip155Address].getMnemonic()}</Text>
                     </Card>
-
-                    <Text h4 css={{ marginTop: '$10', marginBottom: '$5' }}>
-                        Cosmos Mnemonic
-                    </Text>
+                    <Text h4 css={{ marginTop: '$10', marginBottom: '$5' }}>Cosmos Mnemonic</Text>
                     <Card bordered borderWeight="light" css={{ minHeight: '100px' }}>
                         <Text css={{ fontFamily: '$mono' }}>{cosmosWallets[cosmosAddress].getMnemonic()}</Text>
                     </Card>
-
-                    <Text h4 css={{ marginTop: '$10', marginBottom: '$5' }}>
-                        Solana Secret Key
-                    </Text>
+                    <Text h4 css={{ marginTop: '$10', marginBottom: '$5' }}>Solana Secret Key</Text>
                     <Card bordered borderWeight="light" css={{ minHeight: '215px', wordWrap: 'break-word' }}>
                         <Text css={{ fontFamily: '$mono' }}>{solanaWallets[solanaAddress].getSecretKey()}</Text>
                     </Card>
-
-                    <Text h4 css={{ marginTop: '$10', marginBottom: '$5' }}>
-                        MultiversX Mnemonic
-                    </Text>
+                    <Text h4 css={{ marginTop: '$10', marginBottom: '$5' }}>MultiversX Mnemonic</Text>
                     <Card bordered borderWeight="light" css={{ minHeight: '215px', wordWrap: 'break-word' }}>
-                        <Text css={{ fontFamily: '$mono' }}>
-                            {multiversxWallets[multiversxAddress].getMnemonic()}
-                        </Text>
+                        <Text css={{ fontFamily: '$mono' }}>{multiversxWallets[multiversxAddress].getMnemonic()}</Text>
                     </Card>
-
-                    <Text h4 css={{ marginTop: '$10', marginBottom: '$5' }}>
-                        Tron Private Key
-                    </Text>
+                    <Text h4 css={{ marginTop: '$10', marginBottom: '$5' }}>Tron Private Key</Text>
                     <Card bordered borderWeight="light" css={{ minHeight: '100px', wordWrap: 'break-word' }}>
                         <Text css={{ fontFamily: '$mono' }}>{tronWallets[tronAddress].privateKey}</Text>
                     </Card>
-
-                    <Text h4 css={{ marginTop: '$10', marginBottom: '$5' }}>
-                        Tezos Mnemonic
-                    </Text>
-                    
+                    <Text h4 css={{ marginTop: '$10', marginBottom: '$5' }}>Tezos Mnemonic</Text>
                     <Card bordered borderWeight="light" css={{ minHeight: '100px', wordWrap: 'break-word' }}>
                         <Text css={{ fontFamily: '$mono' }}>{tezosWallets[tezosAddress].getMnemonic()}</Text>
                     </Card>
-
-                    {/*<Text h4 css={{ marginTop: '$10', marginBottom: '$5' }}>*/}
-                    {/*    Kadena Secret Key*/}
-                    {/*</Text>*/}
-                    
-                    {/*<Card bordered borderWeight="light" css={{ wordWrap: 'break-word' }}>*/}
-                    {/*    <Text css={{ fontFamily: '$mono' }}>{kadenaWallets[kadenaAddress].getSecretKey()}</Text>*/}
-                    {/*</Card>*/}
-
                     <Text h4 css={{ marginTop: '$10', marginBottom: '$5' }}></Text>
                 </Fragment>
             )}
         </Fragment>
-    )
+    );
 }
